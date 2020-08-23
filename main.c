292aec9fe047c1ccb0a522a473c970dccdc637c3
Hydroponey <hydroponey@localhost>
2020-08-23 03:58:37 +0200

    Add new hook function to handle clicks

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/23 01:50:11 by asimoes           #+#    #+#             */
-/*   Updated: 2020/08/23 03:55:14 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/08/23 03:58:21 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,17 @@ typedef struct   s_vars {
     void    *win;
 }               t_vars;
 
-int exit_hook(int keycode, t_vars *vars)
+int escape_key_hook(int keycode, t_vars *vars)
 {
-    printf("Exit called\n");
+    printf("ESC pressed\n");
     mlx_destroy_window(vars->mlx, vars->win);
     exit(0);
+}
+
+int red_cross_hook(int button, int x, int y, t_vars *vars)
+{
+    printf("Click on button: %d\n", button);
+//    exit(0);
 }
 
 int main(void)
@@ -36,8 +42,8 @@ int main(void)
     vars.win = mlx_new_window(vars.mlx, 640, 480, "Cub3D");
     if (!vars.win)
         return (0);
-    mlx_hook(vars.win, 2, 1L<<0, exit_hook, &vars);
-    mlx_hook(vars.win, 17, 1L<<2, exit_hook, &vars);
+    mlx_hook(vars.win, 2, 1L<<0, escape_key_hook, &vars);
+    mlx_hook(vars.win, 17, 1L<<2, red_cross_hook, &vars);
     mlx_loop(vars.mlx);
 
     return (0);
