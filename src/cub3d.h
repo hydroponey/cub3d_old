395718652cda02e4f6c2487499e278510222370e
Hydroponey <hydroponey@localhost>
2020-08-28 23:23:15 +0200

    cub3d.h fix some norme errors

--- src/cub3d.h
+++ src/cub3d.h
@@ -1,13 +1,13 @@
 /* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/08/28 23:20:14 by asimoes           #+#    #+#             */
-/*   Updated: 2020/08/28 23:20:14 by asimoes          ###   ########.fr       */
-/*                                                                            */
+/*																			*/
+/*														:::	  ::::::::   */
+/*   cub3d.h											:+:	  :+:	:+:   */
+/*													+:+ +:+		 +:+	 */
+/*   By: asimoes <asimoes@student.42.fr>			+#+  +:+	   +#+		*/
+/*												+#+#+#+#+#+   +#+		   */
+/*   Created: 2020/08/28 23:20:14 by asimoes		   #+#	#+#			 */
+/*   Updated: 2020/08/28 23:20:14 by asimoes		  ###   ########.fr	   */
+/*																			*/
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
@@ -27,32 +27,32 @@
 #define TEXTURE_WE	2
 #define TEXTURE_EA	3
 
-typedef struct  s_reso {
-    int	x;
-    int	y;
-}               t_reso;
+typedef struct	s_reso {
+	int	x;
+	int	y;
+}				t_reso;
 
-typedef struct  s_conf {
-    char        *map_path;
-    int         save_bmp;
-    int         map_fd;
-    t_reso      resolution;
-    char		*textures[5];
-    int      	floor_color[3];
-    int      	ceil_color[3];
-}               t_conf;
+typedef struct	s_conf {
+	char	*map_path;
+	int		save_bmp;
+	int		map_fd;
+	t_reso	resolution;
+	char	*textures[5];
+	int		floor_color[3];
+	int		ceil_color[3];
+}				t_conf;
 
-typedef struct  s_vars {
-    void    *mlx;
-    void    *win;
-}               t_vars;
+typedef struct	s_vars {
+	void	*mlx;
+	void	*win;
+}				t_vars;
 
 int				is_param(char *data, int param_id);
 void			ft_freetab(char **tab);
 int				is_numeric(char *str);
 
 int				check_args(t_conf **conf, int argc, char **argv);
-int     		parse_config(t_conf *conf);
+int	 		parse_config(t_conf *conf);
 void			free_config(t_conf *conf);
 
 int				get_resolution(t_conf *conf, char **conf_strings);
