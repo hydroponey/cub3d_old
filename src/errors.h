6c564c66e014a9c5db3470e4d9ab38d2ac7a42c1
Hydroponey <hydroponey@localhost>
2020-08-29 23:23:49 +0200

    fix errors.h norme

--- src/errors.h
+++ src/errors.h
@@ -6,34 +6,34 @@
 /*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/25 17:53:27 by asimoes           #+#    #+#             */
-/*   Updated: 2020/08/28 22:36:21 by asimoes          ###   ########.fr       */
+/*   Updated: 2020/08/29 23:23:34 by asimoes          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ERRORS_H
-#define ERRORS_H
+# define ERRORS_H
 
-#define ERR_SUCCESS					0
+# define ERR_SUCCESS				0
 
-#define ERR_CMD_USAGE				1
-#define ERR_CMD_BAD_EXT				2
-#define ERR_CMD_OPEN_FAIL			3
+# define ERR_CMD_USAGE				1
+# define ERR_CMD_BAD_EXT			2
+# define ERR_CMD_OPEN_FAIL			3
 
-#define ERR_MALLOC_CUBE				4
+# define ERR_MALLOC_CUBE			4
 
-#define ERR_SPLIT_FAILED			5
-#define ERR_SPLIT_VALUES			6
+# define ERR_SPLIT_FAILED			5
+# define ERR_SPLIT_VALUES			6
 
-#define ERR_R_NOT_FOUND				7
-#define ERR_R_NON_NUMERIC			8
+# define ERR_R_NOT_FOUND			7
+# define ERR_R_NON_NUMERIC			8
 
-#define ERR_GNL_FAIL				9
-#define ERR_TEXTURE_NOT_FOUND		10
-#define ERR_TEXTURE_EMPTY			11
-#define ERR_CEIL_COLOR_NOT_FOUND	12
-#define ERR_FLOOR_COLOR_NOT_FOUND	13
-#define ERR_CEIL_NON_NUMERIC		14
-#define ERR_FLOOR_NON_NUMERIC		15
-#define ERR_CEIL_WRONG_RGB_VALUE	16
+# define ERR_GNL_FAIL				9
+# define ERR_TEXTURE_NOT_FOUND		10
+# define ERR_TEXTURE_EMPTY			11
+# define ERR_CEIL_COLOR_NOT_FOUND	12
+# define ERR_FLOOR_COLOR_NOT_FOUND	13
+# define ERR_CEIL_NON_NUMERIC		14
+# define ERR_FLOOR_NON_NUMERIC		15
+# define ERR_CEIL_WRONG_RGB_VALUE	16
 
 #endif
