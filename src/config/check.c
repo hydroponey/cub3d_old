8b59c092ee5a221f5f3deb30880e6ea11cdf0af9
Hydro Poney <hydro@poney>
2020-09-18 12:54:59 +0200

    Move config parsing files into config directory

---
+++ src/config/check.c
@@ -0,0 +1,141 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/04 20:06:36 by asimoes           #+#    #+#             */
+/*   Updated: 2020/09/18 12:13:53 by asimoes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include "libft/libft.h"
+#include "../cub3d.h"
+#include "../errors.h"
+
+int						check_args(t_conf **conf, int argc, char **argv)
+{
+	int		path_len;
+	int		save_bmp;
+	int		fd;
+	int		err;
+	int		ret;
+
+	fd = -1;
+	err = 0;
+	*conf = NULL;
+	if (argc != 2 && argc != 3)
+		err = ERR_CMD_USAGE;
+	if (!err && (path_len = ft_strlen(argv[1])) < 4)
+		err = ERR_CMD_USAGE;
+	if (!err && ft_strncmp(&argv[1][path_len - 4], ".cub", 4) != 0)
+		err = ERR_CMD_BAD_EXT;
+	if (!err && argc == 3 && (save_bmp = ft_strncmp(argv[2], "--save", 6)) != 0)
+		err = ERR_CMD_USAGE;
+	if (!err && (fd = open(argv[1], O_RDONLY)) == -1)
+		err = ERR_CMD_OPEN_FAIL;
+	if (!err && (ret = setup_config(conf, argv, fd, save_bmp)) != 0)
+		err = ret;
+	return (err);
+}
+
+int						trim_end_of_map(t_conf *conf)
+{
+	int			err;
+	short int	is_empty;
+	char		*trimmed;
+
+	err = ERR_SUCCESS;
+	is_empty = 1;
+	while (is_empty)
+	{
+		if (!(trimmed = ft_strtrim(conf->map_text[conf->map_lines - 1], " \t")))
+		{
+			err = ERR_MALLOC_CUBE;
+			break ;
+		}
+		if (ft_strlen(trimmed) == 0)
+		{
+			free(conf->map_text[conf->map_lines - 1]);
+			conf->map_lines--;
+		}
+		else
+			is_empty = 0;
+		free(trimmed);
+	}
+	return (err);
+}
+
+int						closed_check(t_conf *conf)
+{
+	int	w;
+	int	h;
+	int	is_border;
+
+	h = 0;
+	while (h < conf->map_dim.y)
+	{
+		w = 0;
+		while (w < conf->map_dim.x)
+		{
+			is_border = h == 0 || w == 0 || h == (conf->map_dim.y - 1) ||
+						w == (conf->map_dim.x - 1);
+			if (is_border && conf->map[h][w] == 0)
+				return (ERR_MAP_NOT_CLOSED);
+			if (conf->map[h][w] == 0)
+			{
+				if (conf->map[h + 1][w] == -1 || conf->map[h][w + 1] == -1)
+					return (ERR_MAP_NOT_CLOSED);
+			}
+			w++;
+		}
+		h++;
+	}
+	return (ERR_SUCCESS);
+}
+
+int						check_map(t_conf *conf)
+{
+	int			err;
+	char		*trimmed;
+
+	err = ERR_SUCCESS;
+	if (conf->map_text == NULL)
+		err = ERR_MAP_NOT_PARSED;
+	if ((err = trim_end_of_map(conf)) != ERR_SUCCESS)
+		return (err);
+	trimmed = ft_strtrim(conf->map_text[conf->map_lines - 1], " 1");
+	if (ft_strlen(trimmed) != 0)
+		err = ERR_BAD_MAP_END;
+	free(trimmed);
+	trimmed = ft_strtrim(conf->map_text[0], " 1");
+	if (ft_strlen(trimmed) != 0)
+		err = ERR_BAD_MAP_START;
+	free(trimmed);
+	err = (!err) ? transform_map(conf) : err;
+	if (!err && (conf->pos.x == -1 || conf->pos.y == -1))
+		err = ERR_START_POS_NOT_FOUND;
+	if (!err && (conf->pos.x == 0 || conf->pos.y == 0))
+		err = ERR_START_POS_ON_BORDER;
+	err = (!err) ? closed_check(conf) : err;
+	return (err);
+}
+
+int						check_config(t_conf *conf, char **conf_strings)
+{
+	int	retval;
+
+	retval = ERR_SUCCESS;
+	retval = get_resolution(conf, conf_strings);
+	retval = (!retval) ? get_textures(conf, conf_strings) : retval;
+	retval = (!retval) ? get_ceil_color(conf, conf_strings) : retval;
+	retval = (!retval) ? get_floor_color(conf, conf_strings) : retval;
+	retval = (!retval) ? check_map(conf) : retval;
+	return (retval);
+}
---
+++ src/config/utils.c
@@ -0,0 +1,116 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asimoes <asimoes@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/28 10:55:57 by asimoes           #+#    #+#             */
+/*   Updated: 2020/09/18 12:13:42 by asimoes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include "libft/libft.h"
+#include "../cub3d.h"
+#include "../errors.h"
+
+int		setup_config(t_conf **conf_ptr, char **argv, int fd, int save_bmp)
+{
+	int		err;
+	t_conf	*conf;
+
+	err = ERR_SUCCESS;
+	conf = (t_conf*)malloc(sizeof(t_conf));
+	if (!conf)
+		return (ERR_MALLOC_CUBE);
+	ft_bzero(conf, sizeof(t_conf));
+	conf->map_path = ft_strdup(argv[1]);
+	if (!conf->map_path)
+		err = ERR_MALLOC_CUBE;
+	conf->mlx = NULL;
+	conf->win = NULL;
+	conf->save_bmp = (save_bmp == 0) ? 1 : 0;
+	conf->map_fd = fd;
+	conf->map = NULL;
+	conf->map_text = NULL;
+	conf->map_lines = 0;
+	conf->pos.x = -1;
+	conf->pos.y = -1;
+	if (err != ERR_SUCCESS)
+	{
+		free(conf);
+		close(fd);
+	}
+	*conf_ptr = conf;
+	return (err);
+}
+
+int		is_param(char *data, int param_id)
+{
+	char			*params[8];
+	unsigned short	param_len;
+
+	params[0] = "R ";
+	params[1] = "NO ";
+	params[2] = "SO ";
+	params[3] = "WE ";
+	params[4] = "EA ";
+	params[5] = "S ";
+	params[6] = "F ";
+	params[7] = "C ";
+	param_len = ft_strlen(params[param_id]);
+	if (ft_strncmp(data, params[param_id], param_len) == 0)
+		return (param_len);
+	return (-1);
+}
+
+void	ft_freetab(char **tab)
+{
+	char **start;
+
+	if (!tab)
+		return ;
+	start = tab;
+	while (*tab)
+		free(*tab++);
+	free(start);
+}
+
+int		is_numeric(char *str)
+{
+	while (*str != '\0')
+	{
+		if (!ft_isdigit(*str))
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+void	free_config(t_conf *conf)
+{
+	int i;
+
+	if (!conf)
+		return ;
+	close(conf->map_fd);
+	free(conf->map_path);
+	free(conf->textures[0]);
+	free(conf->textures[1]);
+	free(conf->textures[2]);
+	free(conf->textures[3]);
+	free(conf->textures[4]);
+	i = 0;
+	while (i < conf->map_lines)
+	{
+		if (conf->map_text != NULL)
+			free(conf->map_text[i]);
+		if (conf->map != NULL)
+			free(conf->map[i]);
+		i++;
+	}
+	free(conf->map_text);
+	free(conf->map);
+	free(conf);
+}
